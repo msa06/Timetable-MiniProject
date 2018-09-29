ac65fd23931e8d3c44b0164a8f073b77c47db828
Mohammad Suhaib Ahmed <38851822+msa06@users.noreply.github.com>
2018-09-29 12:46:42 +0530

    Adding toolbar

--- app/src/main/java/com/example/msa/timetable/Activities/AboutActivity.java
+++ app/src/main/java/com/example/msa/timetable/Activities/AboutActivity.java
@@ -1,7 +1,9 @@
 package com.example.msa.timetable.Activities;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import com.example.msa.timetable.R;
@@ -13,6 +15,10 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Toolbar mytoolbar = (Toolbar) findViewById(R.id.abouttoolbar);
+        mytoolbar.setTitle("Add Period");
+        setSupportActionBar(mytoolbar);
+        ActionBar ab = getSupportActionBar();
 
         listView=(ListView)findViewById(R.id.listview);
         ArrayAdapter<CharSequence> aa = ArrayAdapter.createFromResource(this, R.array.Member_info, android.R.layout.simple_list_item_1);
